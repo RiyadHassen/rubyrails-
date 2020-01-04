1494b5a4db63115f485dabf02e0bce88814f8b72
RiyadHassen <riyadworku@gmail.com>
2020-01-04 03:25:44 +0300

    gretting controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'gretter/hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get 'gretter/goodbye'
 end
